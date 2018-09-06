6e5fa179b36000b2da9e020c1a91d3be5a91f9ab
wyx <2214252994@qq.com>
2018-09-06 16:32:30 +0800

    asdfasfasfasfdasdfsafsafaf

--- testDemo/src/testDemo/test.java
+++ testDemo/src/testDemo/test.java
@@ -5,12 +5,13 @@ import redis.clients.jedis.Jedis;
 public class test {
 	
   public static void main(String[] args) {
-      //连接本地的 Redis 服务
+      //连接本地的 Redis 服务  sdfsfsasfsa
       Jedis jedis = new Jedis("localhost");
+      ///alsjdfla
       System.out.println("连接成功");
       //设置 redis 字符串数据
       jedis.set("runoobkey", "www.runoob.com");
-      // 获取存储的数据并输出
+      // 获取存储的数据并输出  asfdasfsa
       System.out.println("redis 存储的字符串为: "+ jedis.get("runoobkey"));
   }
   
